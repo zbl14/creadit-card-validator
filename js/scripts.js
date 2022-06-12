e5754ae677f9192a6e3e8450ebd5f7eb0adb46de
zbl14 <ifthereisoneday@gmail.com>
2022-06-12 09:10:01 -0700

    remove nonnumber input, creat numArr, reverse the oreder of this arr, test passed

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+function luhnRule(num){
+  let numArr = num.toString().replace(/[^0-9]/g,"").split("").reverse();
+  let uncheckedNumArr = [];
+  let checkedNumArr = [];
+  for (i = 0; i < numArr.length; i++) {
+    if (i % 2 === 0) {
+      checkedNumArr.push(parseInt(numArr[i]));
+    } else {
+      uncheckedNumArr.push(parseInt(numArr[i])); 
+    }
+  }
+  let doubleCheckedNumArr = checkedNumArr.map(function(checkedNum){
+    if (checkedNum * 2 > 9) {
+      return checkedNum * 2 - 9;
+    } else {
+      return checkedNum * 2;
+    }
+  });
+  const sumAll = uncheckedNumArr.reduce((a,b) => a+b, 0) + doubleCheckedNumArr.reduce((a,b) => a+b, 0);
+  return sumAll
+}
